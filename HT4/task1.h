be1c75ef1a08138f751d66a627bcb5aa51c9a1d3
AleksandrRagulin <80351648+AleksandrRagulin@users.noreply.github.com>
2021-11-04 18:47:30 +0300

    Lesson4

---
+++ HT4/task1.h
@@ -0,0 +1,14 @@
+#ifndef HT4_TASK1_H
+#define HT4_TASK1_H
+
+#include <vector>
+#include <iostream>
+
+void task1();
+
+template<typename T>
+void insert_sorted(std::vector <T> &vec, T x);
+
+template<typename T>
+void print_vector(const std::vector <T> &vec);
+#endif //HT4_TASK1_H
---
+++ HT4/task2.h
@@ -0,0 +1,17 @@
+#ifndef HT4_TASK2_H
+#define HT4_TASK2_H
+
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+
+void task2();
+
+template<typename T>
+void print_vector(const std::vector <T> &vec);
+
+
+#endif //HT4_TASK2_H
